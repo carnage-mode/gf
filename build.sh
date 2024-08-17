c793209dbaad5f42a62be83404f730b12be6a955
Beamanuel Tesfaye <beta-stage@tutanota.com>
2024-08-17 22:31:08 +0300

    Make the bulid script work for my local dev setup

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ gdb --version > /dev/null 2>&1 || exit 1
 echo q | gdb | grep "(gdb)" > /dev/null 2>&1 || printf "\033[0;31mWarning\033[0m: Your copy of GDB appears to be non-standard or has been heavily reconfigured with .gdbinit.\nIf you are using GDB plugins like 'GDB Dashboard' you must remove them,\nas otherwise gf will be unable to communicate with GDB.\n"
 
 # Check if FreeType is available.
-if [ -d /usr/include/freetype2 ]; then extra_flags="$extra_flags -lfreetype -D UI_FREETYPE -I /usr/include/freetype2"; 
+if [ -d $HOMEBREW_INCLUDE/freetype2 ]; then extra_flags="$extra_flags -lfreetype -D UI_FREETYPE -I $HOMEBREW_INCLUDE/freetype2"; 
 else printf "\033[0;31mWarning\033[0m: FreeType could not be found. The fallback font will be used.\n"; fi
 
 # Check if SSE2 is available.
@@ -14,3 +14,6 @@ uname -m | grep x86_64 > /dev/null && extra_flags="$extra_flags -DUI_SSE2"
 
 # Build the executable.
 g++ gf2.cpp -o gf2 -g -O2 -lX11 -pthread $extra_flags -Wall -Wextra -Wno-unused-parameter -Wno-unused-result -Wno-missing-field-initializers -Wno-format-truncation || exit 1
+
+# Move to ~/.local/bin/
+mv ./gf2 $HOME/.local/bin
